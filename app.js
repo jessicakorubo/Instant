498f719719cb57863f5f85930a2d5fe8cdfda2d6
Jessica Korubo <jessicakorubo@gmail.com>
2024-06-18 17:29:27 +0100

    done

---
+++ app.js
@@ -0,0 +1,46 @@
+function Calculator(num1, num2) {
+    num1 = this.num1;
+    num2 = this.num2;
+   
+    this.read = function(){
+        num1 = prompt("Enter num 1");
+        num2 = prompt ("Enter num 2");
+    }
+
+    this.sum = function(){
+        return this.num1 + this.num2;
+    }
+    this.mul = function() {
+        return  num1* num2;
+    }
+}
+
+function Accumulator(startingValue){
+    this.startingValue = startingValue;
+
+    this.read() = function (){
+        this.value =+ prompt("Enter value here", 0);
+    }
+}
+
+function isArray(myArray)  {
+    return myArray.constructor.toString().indexOf("Array") > -1
+}
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.mobile-menu');
+
+
+hamburger.addEventListener('click', ()=> {
+    console.log('clicked on hamburger!');
+    const style = menu.computedStyleMap().get('display').value;
+
+    if (style == 'block') {
+        menu.style.display = 'none';
+    }
+    else if (style == 'none') {
+        menu.style.display = 'block';
+    }
+})
+
+// show
